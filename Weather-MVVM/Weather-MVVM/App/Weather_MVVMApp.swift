76fc985703a2645ea6a9127d4e6d9cdcdfb2a95b
Gerardo Ramos <gerx_37@hotmail.com>
2026-03-01 12:53:12 -0700

    Created project

---
+++ Weather-MVVM/Weather-MVVM/App/Weather_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_MVVMApp.swift
+//  Weather-MVVM
+//
+//  Created by Gerardo Ramos on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Weather_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-MVVM/Weather-MVVM/Models/WeatherModel.swift
@@ -0,0 +1,10 @@
+//
+//  WeatherModel.swift
+//  Weather-MVVM
+//
+//  Created by Gerardo Ramos on 28/02/26.
+//
+
+struct WeatherModel: Decodable {
+
+}
